ae03f6b43e5db3ca95878cfe1cddad64fadcd44f
Huy <duchuykun@gmail.com>
2016-12-18 10:55:04 +0700

    Update example

--- Mapper/AppDelegate.swift
+++ Mapper/AppDelegate.swift
@@ -12,14 +12,18 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let model = Model()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        let model = Model(dictionary: ["Name": "Carrot", "ID": "A2jsdk"])
-        
-        print(model.toDictionary())
+        model.property("Name", target: self, selector: #selector(AppDelegate.ChangeName), on: .onChange)
+        model.initData(["Name": "Carrot", "ID": "A2jsdk"])
         
         return true
+    }
+    
+    func ChangeName() {
+        print(model.name)
     }
 }
 
