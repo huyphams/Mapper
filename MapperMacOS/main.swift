9394ac977ccd8ed062451790ed2037ac50ebcaea
Huy Pham <duchuykun@gmail.com>
2019-01-16 13:26:50 +0700

    Added macOS.

---
+++ MapperMacOS/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MapperMacOS
+//
+//  Created by Pham Huy on 1/16/19.
+//  Copyright © 2019 Huy Pham. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
